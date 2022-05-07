22e1860a40dbb8b491144a2b517f108aeca2d4f8
melodyn <samelodyn@gmail.com>
2022-05-07 15:44:07 +0300

    added js, rename modal to popup

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const popupEl = document.querySelector('.popup');
+  const buttonEls = document.querySelectorAll('.button[type="button"]');
+  const getProfileTitleEl = () => document.querySelector('.profile__title-text');
+  const getProfileSubtitleEl = () => document.querySelector('.profile__subtitle');
+  const formEl = popupEl.querySelector('.popup-form');
+
+  const profileEditHandler = () => {
+    const profileTitleEl = getProfileTitleEl();
+    const profileSubtitleEl = getProfileSubtitleEl();
+    formEl.title.value = profileTitleEl.textContent;
+    formEl.subtitle.value = profileSubtitleEl.textContent;
+    popupEl.classList.add('popup_opened');
+  };
+
+  const closePopupHandler = () => {
+    popupEl.classList.remove('popup_opened');
+  };
+
+  const likeHandler = (buttonEl) => () => {
+    if (buttonEl.classList.contains('place__like_liked')) {
+      buttonEl.classList.remove('place__like_liked');
+    } else {
+      buttonEl.classList.add('place__like_liked');
+    }
+  };
+
+  formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get('title');
+    const subtitle = formData.get('subtitle');
+
+    const profileTitleEl = getProfileTitleEl();
+    const profileSubtitleEl = getProfileSubtitleEl();
+    profileTitleEl.textContent = title;
+    profileSubtitleEl.textContent = subtitle;
+
+    popupEl.classList.remove('popup_opened');
+  });
+
+  buttonEls.forEach((button) => {
+    const { classList } = button;
+    switch (true) {
+      case classList.contains('profile__edit'):
+        button.addEventListener('click', profileEditHandler);
+        break;
+      case classList.contains('popup__close'):
+        button.addEventListener('click', closePopupHandler);
+        break;
+      case classList.contains('place__like'):
+        button.addEventListener('click', likeHandler(button));
+        break;
+      default:
+        button.addEventListener('click', () => alert('Папався! 🦀'));
+        break;
+    }
+  });
+});
