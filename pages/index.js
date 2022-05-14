35c9cc58e938695ffbe6855ec2808e03ca285a6c
melodyn <samelodyn@gmail.com>
2022-05-14 16:59:03 +0300

    places refactoring
    rewrite from HTML to JS, added template, added button for remove

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
+const initCards = [
+  {
+    name: 'Алматы',
+    link: 'images/almaty.jpg',
+  },
+  {
+    name: 'Алтай',
+    link: 'images/altai.jpg',
+  },
+  {
+    name: 'Челябинск',
+    link: 'images/chelyabynsk.jpg',
+  },
+  {
+    name: 'Ленинградская область',
+    link: 'images/leningrad.jpg',
+  },
+  {
+    name: 'Мурманск',
+    link: 'images/murmansk.jpg',
+  },
+  {
+    name: 'Тобольск',
+    link: 'images/tobolsk.jpg',
+  },
+];
+
 document.addEventListener('DOMContentLoaded', () => {
   const popupEl = document.querySelector('.popup');
+  const formEl = popupEl.querySelector('.popup-form');
   const buttonEls = document.querySelectorAll('.button');
   const profileTitleEl = document.querySelector('.profile__title');
   const profileSubtitleEl = document.querySelector('.profile__subtitle');
-  const formEl = popupEl.querySelector('.popup-form');
 
   const profileEditHandler = () => {
     formEl.title.value = profileTitleEl.textContent;
@@ -14,14 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const closePopupHandler = () => {
     popupEl.classList.remove('popup_opened');
-  };
-
-  const likeHandler = (buttonEl) => () => {
-    if (buttonEl.classList.contains('place__like_liked')) {
-      buttonEl.classList.remove('place__like_liked');
-    } else {
-      buttonEl.classList.add('place__like_liked');
-    }
   };
 
   const submitHandler = (e) => {
@@ -58,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
           button.addEventListener('click', closePopupHandler);
           break;
         case classList.contains('place__like'):
-          button.addEventListener('click', likeHandler(button));
+          // button.addEventListener('click', likeHandler(button));
           break;
         default:
           button.addEventListener('click', defaultClickHandler);
@@ -66,4 +85,53 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
+
+  /* place */
+  const placesListEl = document.querySelector('.places__list');
+  const placeTemplateEl = document
+    .querySelector('#place')
+    .content
+    .querySelector('.place');
+
+  const likeHandler = (likeEl) => () => {
+    if (likeEl.classList.contains('place__like_liked')) {
+      likeEl.classList.remove('place__like_liked');
+    } else {
+      likeEl.classList.add('place__like_liked');
+    }
+  };
+
+  const removeHandler = (cardEl) => () => {
+    cardEl.remove();
+  };
+
+  const createPlace = (containerEl, place) => {
+    const placeContainerEl = containerEl.querySelector('.place');
+    const imgEl = placeContainerEl.querySelector('.place__image');
+    const linkEl = placeContainerEl.querySelector('.place__link');
+    const likeEl = placeContainerEl.querySelector('.place__like');
+    const removeEl = placeContainerEl.querySelector('.place__remove');
+
+    placeContainerEl.setAttribute('aria-label', place.name);
+
+    imgEl.setAttribute('alt', place.name);
+    imgEl.setAttribute('src', place.link);
+
+    linkEl.setAttribute('href', place.link);
+    linkEl.textContent = place.name;
+
+    likeEl.addEventListener('click', likeHandler(likeEl));
+    removeEl.addEventListener('click', removeHandler(containerEl));
+  };
+
+  const addPlace = (place) => {
+    const liEl = document.createElement('li');
+    const placeEl = placeTemplateEl.cloneNode(true);
+    liEl.append(placeEl);
+    createPlace(liEl, place);
+    placesListEl.append(liEl);
+  };
+
+  /* run */
+  initCards.forEach(addPlace);
 });
